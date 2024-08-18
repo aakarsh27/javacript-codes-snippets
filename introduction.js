04408f2dc38a103f2d811d35b7fbea5f72614d76
Aakarsh Sharma <71917139+aakarsh27@users.noreply.github.com>
2024-08-18 21:41:27 +0530

    Add files via upload

---
+++ introduction.js
@@ -0,0 +1,26 @@
+console.log("Hello my name is aakarsh")
+console.log('this is my time using html')
+age = '21'
+console.log(age)
+fullname = "aakarsh"
+console.log(fullname)
+
+
+// always use let, var, or const to define a variable in javascript, it shouldnt be done directly
+
+
+const student = {
+    fullName : 'aakarsh',
+    age: 20,
+    cgpa: 5.5,
+    isPass: true,
+};
+
+const product = {
+    nameofprod: "Parker Jotter Standard CT Ball Pen (Black)",
+    price: 270,
+    rating: 4,
+    offer: 5,
+}
+
+console.log(typeof product["nameofprod"]); 
